1c190c05970739d8d3051a05f57d1cfa00399d78
Mark Skelton <mdskelton99@gmail.com>
2021-10-26 12:06:21 -0500

    Next 12 (#1)
    * Update next
    
    * Add incremental flag
    
    * Add next ESLint config and swc minify
    
    * Update ESLint config
    
    * remove dep

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, {
+import NextDocument, {
   DocumentContext,
   Head,
   Html,
@@ -6,9 +6,9 @@ import Document, {
   NextScript,
 } from "next/document"
 
-export default class extends Document {
+export default class Document extends NextDocument {
   static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
+    const initialProps = await NextDocument.getInitialProps(ctx)
     return { ...initialProps }
   }
 
