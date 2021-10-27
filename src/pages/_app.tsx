b7eea5508c019cbc7320ff9599b870573408feeb
Mark Skelton <mdskelton99@gmail.com>
2021-10-27 07:56:36 -0500

    Add Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
 import "../css/style.css"
 import type { AppProps } from "next/app"
+import Script from "next/script"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const isLocal = process.env.NODE_ENV !== "production"
+  const measurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
+
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${measurementId}', { debug_mode: ${isLocal}});
+          gtag('consent', 'default', { analytics_storage: 'denied' });
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
