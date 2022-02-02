5c2b5325771ca8c763aec4efb344af2212acbc4e
Mark Skelton <mdskelton99@gmail.com>
2022-02-01 23:40:40 -0600

    Add Remix config

---
+++ remix.config.js
@@ -0,0 +1,8 @@
+/** @type {import('@remix-run/dev/config').AppConfig} */
+module.exports = {
+  appDirectory: "src",
+  browserBuildDirectory: "public/build",
+  devServerPort: 8002,
+  publicPath: "/build/",
+  serverBuildDirectory: "build",
+}
