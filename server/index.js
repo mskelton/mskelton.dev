ace552aae17839527a75045cd497499e5fc9371d
Mark Skelton <mdskelton99@gmail.com>
2022-02-10 08:08:34 -0600

    Finish redirects

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const remix = require("@remix-run/express")
 const express = require("express")
 const path = require("path")
-// const { getRedirectMiddleware } = require("./middleware/redirects")
+const { getRedirectMiddleware } = require("./middleware/redirects")
 
 const app = express()
 
 app.use(express.static(path.join(__dirname, "../public")))
-// app.use(getRedirectMiddleware())
+app.use(getRedirectMiddleware())
 app.all("*", remix.createRequestHandler({ build: require("./build") }))
 
 app.listen(3000, () => {
--- server/middleware/redirects.js
+++ server/middleware/redirects.js
@@ -1,60 +1,16 @@
 const fs = require("fs")
 const path = require("path")
+const yaml = require("js-yaml")
 
 exports.getRedirectMiddleware = () => {
-  const source = fs.readFileSync(path.join(__dirname, "../_redirects"), "utf8")
+  const filePath = path.join(__dirname, "../config/redirects.yaml")
+  const redirects = yaml.load(fs.readFileSync(filePath, "utf8"))
 
-  const redirects = source
-    .split("\n")
-    .map((line, lineNumber) => {
-      if (!line.trim() || line.startsWith("#")) return null
+  return function redirectMiddleware(req, res, next) {
+    const redirect = redirects[req.url]
 
-      const [from, to] = line
-        .split(" ")
-        .map((part) => part.trim())
-        .filter(Boolean)
-
-      if (!from || !to) {
-        console.error(`Invalid redirect on line ${lineNumber + 1}: "${line}"`)
-        return null
-      }
-
-      const toURL = to.startsWith("/")
-        ? new URL(to, "https://mskelton.dev")
-        : new URL(to)
-
-      return { from, toURL }
-    })
-    .filter(Boolean)
-
-  console.log(redirects)
-
-  return function redirectsMiddleware(req, res, next) {
-    const host = req.header("X-Forwarded-Host") ?? req.header("host")
-    const protocol = host?.includes("localhost") ? "http" : "https"
-    let reqUrl
-
-    try {
-      reqUrl = new URL(`${protocol}://${host}${req.url}`)
-    } catch (error) {
-      console.error(`Invalid URL: ${protocol}://${host}${req.url}`)
-      next()
-      return
-    }
-
-    for (const redirect of redirects) {
-      const match = req.path === redirect.from
-      if (!match) continue
-
-      const toUrl = redirect.toUrl
-      toUrl.protocol = protocol
-      if (toUrl.host === "same_host") toUrl.host = reqUrl.host
-
-      for (const [key, value] of reqUrl.searchParams.entries()) {
-        toUrl.searchParams.append(key, value)
-      }
-      toUrl.pathname = redirect.toPathname(params)
-      res.redirect(307, toUrl.toString())
+    if (redirect) {
+      res.redirect(307, redirect)
       return
     }
 
