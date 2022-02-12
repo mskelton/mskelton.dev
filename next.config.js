d8a088b33a2346eea212c096d6a9f61530c742de
Mark Skelton <mdskelton99@gmail.com>
2022-02-12 14:57:06 -0600

    Longer caching for post images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+/** @type {import('next').NextConfig} */
 module.exports = {
   eslint: {
     dirs: ["src"],
+  },
+  images: {
+    minimumCacheTTL: 3600, // 1 hour
   },
   pageExtensions: ["ts", "tsx", "md", "mdx"],
   reactStrictMode: true,
