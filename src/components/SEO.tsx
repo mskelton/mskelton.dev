367e65c7a55da87aa7ca91710914dd6b010d2767
Mark Skelton <mdskelton99@gmail.com>
2022-08-18 07:38:35 -0500

    Fix lint

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head"
 import { useRouter } from "next/router"
 import metadata from "data/metadata"
-import { AuthorFrontMatter, PostFrontMatter } from "types/FrontMatter"
+import { PostFrontMatter } from "types/FrontMatter"
 
 interface CommonSEOProps {
   description: string
