b5d12a00af1073dc52e1df95f26928cce1408fd7
Mark Skelton <mdskelton99@gmail.com>
2023-02-01 23:36:36 -0600

    Update nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -98,17 +98,14 @@ function NavItem({ children, href }: NavItemProps) {
     <li>
       <Link
         className={clsx(
-          "relative block px-3 py-2 transition",
+          "relative block rounded-full py-1 px-4 transition-colors",
           isActive
-            ? "text-teal-500 dark:text-teal-400"
+            ? "bg-teal-200 dark:bg-teal-800"
             : "hover:text-teal-500 dark:hover:text-teal-400"
         )}
         href={href}
       >
         {children}
-        {isActive && (
-          <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0" />
-        )}
       </Link>
     </li>
   )
@@ -117,7 +114,7 @@ function NavItem({ children, href }: NavItemProps) {
 function DesktopNavigation(props: React.HTMLAttributes<HTMLElement>) {
   return (
     <nav {...props}>
-      <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+      <ul className="flex gap-1 rounded-full bg-white/90 p-2 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
         <NavItem href="/blog">Blog</NavItem>
         <NavItem href="/about">About</NavItem>
         <NavItem href="/projects">Projects</NavItem>
