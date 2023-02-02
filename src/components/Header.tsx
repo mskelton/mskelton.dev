b138e2bbd654fa9c879ff862543ada9a13415bc4
Mark Skelton <mdskelton99@gmail.com>
2023-02-02 08:17:04 -0600

    Design updates (#22)
    * Update nav
    
    * Update home page image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -98,17 +98,14 @@ function NavItem({ children, href }: NavItemProps) {
     <li>
       <Link
         className={clsx(
-          "relative block px-3 py-2 transition",
+          "relative block rounded-full py-1 px-4 transition-colors",
           isActive
-            ? "text-teal-500 dark:text-teal-400"
+            ? "bg-teal-200 dark:bg-teal-800"
             : "hover:text-teal-500 dark:hover:text-teal-400"
         )}
         href={href}
       >
         {children}
-        {isActive && (
-          <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0" />
-        )}
       </Link>
     </li>
   )
@@ -117,7 +114,7 @@ function NavItem({ children, href }: NavItemProps) {
 function DesktopNavigation(props: React.HTMLAttributes<HTMLElement>) {
   return (
     <nav {...props}>
-      <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+      <ul className="flex gap-1 rounded-full bg-white/90 p-2 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
         <NavItem href="/blog">Blog</NavItem>
         <NavItem href="/about">About</NavItem>
         <NavItem href="/projects">Projects</NavItem>
@@ -194,10 +191,10 @@ function Avatar({ className, large = false, ...props }: AvatarProps) {
         alt=""
         className={clsx(
           "rounded-full bg-zinc-100 object-cover dark:bg-zinc-800",
-          large ? "h-16 w-16" : "h-9 w-9"
+          large ? "h-24 w-24" : "h-9 w-9"
         )}
         priority
-        sizes={large ? "4rem" : "2.25rem"}
+        sizes={large ? "6rem" : "2.25rem"}
         src={avatarImage}
       />
     </Link>
@@ -229,7 +226,7 @@ export function Header() {
                       transform: "var(--avatar-border-transform)",
                     }}
                   />
-                  <Avatar className="block h-16 w-16 origin-left" large />
+                  <Avatar className="block h-24 w-24 origin-left" large />
                 </div>
               </div>
             </Container>
---
+++ src/components/Newsletter.tsx
@@ -0,0 +1,34 @@
+import { Button } from "./Button"
+import { MailIcon } from "./icons"
+
+export function Newsletter() {
+  return (
+    <form
+      action="/thank-you"
+      className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40"
+    >
+      <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
+        <MailIcon className="h-6 w-6 flex-none" />
+        <span className="ml-3">Stay up to date</span>
+      </h2>
+
+      <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+        Get notified when I publish something new, and unsubscribe at any time.
+      </p>
+
+      <div className="mt-6 flex">
+        <input
+          aria-label="Email address"
+          className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm"
+          placeholder="Email address"
+          required
+          type="email"
+        />
+
+        <Button className="ml-4 flex-none" type="submit">
+          Join
+        </Button>
+      </div>
+    </form>
+  )
+}
