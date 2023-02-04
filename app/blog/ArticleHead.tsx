ecf36e2ec0bfe68d7f00d30a741c864e7ed6fbaf
Mark Skelton <mdskelton99@gmail.com>
2023-02-04 07:53:42 -0600

    cleanup

--- app/blog/ArticleHead.tsx
+++ app/blog/ArticleHead.tsx
@@ -4,7 +4,7 @@ import { ArticleMeta } from "components/ArticleLayout"
 export function ArticleHead({ meta }: ArticleHeadProps) {
   return (
     <>
-      <title>{`${meta.title} - Mark Skelton's Blog`}</title>
+      <title>{`${meta.title} - Mark Skelton’s Blog`}</title>
       <meta content={meta.description} name="description" />
     </>
   )
