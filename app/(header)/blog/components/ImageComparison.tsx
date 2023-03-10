180303bfc6821c29353f3dfee9db1d1917ac0ae9
Mark Skelton <mdskelton99@gmail.com>
2023-03-10 07:17:18 -0600

    Fix imports

--- app/(header)/blog/components/ImageComparison.tsx
+++ app/(header)/blog/components/ImageComparison.tsx
@@ -1,8 +1,47 @@
-import { getMetadata } from "../meta"
-import Content, { meta } from "./content.mdx"
+"use client"
 
-export const metadata = getMetadata(meta)
+import Image from "next/image"
+import React from "react"
+import {
+  ReactCompareSlider,
+  ReactCompareSliderHandle,
+  ReactCompareSliderHandleProps,
+  ReactCompareSliderImageProps,
+  styleFitContainer,
+} from "react-compare-slider"
 
-export default function Page() {
-  return <Content />
+const Handle = ReactCompareSliderHandle as React.FC<
+  ReactCompareSliderHandleProps & { className: string }
+>
+
+function CompareImage({ alt, src }: React.ImgHTMLAttributes<HTMLImageElement>) {
+  return (
+    <Image
+      alt={alt ?? ""}
+      placeholder="blur"
+      src={require(`../../../images/blog/${src}`)}
+      style={styleFitContainer()}
+    />
+  )
+}
+
+export function ImageComparison({ one, two }: ImageComparisonProps) {
+  return (
+    <ReactCompareSlider
+      handle={
+        <Handle
+          buttonStyle={{ borderColor: "currentcolor" }}
+          className="animate-[delay-appear_200ms_linear_200ms_forwards]	text-teal-500 opacity-0 transition"
+          style={{ color: undefined }}
+        />
+      }
+      itemOne={<CompareImage {...one} />}
+      itemTwo={<CompareImage {...two} />}
+    />
+  )
+}
+
+export interface ImageComparisonProps {
+  one: ReactCompareSliderImageProps
+  two: ReactCompareSliderImageProps
 }
