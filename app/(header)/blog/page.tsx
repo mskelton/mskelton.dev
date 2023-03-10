9585397951f087a00077dd4efe349ed8b432bcd3
Mark Skelton <mdskelton99@gmail.com>
2023-03-09 23:33:16 -0600

    Remove another head.tsx usage

--- app/(header)/blog/page.tsx
+++ app/(header)/blog/page.tsx
@@ -1,8 +1,13 @@
+import { Metadata } from "next"
 import { Card } from "../../components/Card"
 import { SimpleLayout } from "../../components/layouts/SimpleLayout"
 import { formatDate } from "../../lib/formatDate"
 import { getAllArticles } from "../../lib/getAllArticles"
-import { description } from "./meta"
+
+export const metadata: Metadata = {
+  description: `All of my blog posts, even the ones that I might not fully agree with anymore. Software engineering is a process of continuous learning, don’t ever think there isn’t more to learn.`,
+  title: "Blog - Mark Skelton",
+}
 
 export interface IArticle {
   date: string
