6b5866034dbdecbb607db700034a7bc28d6645ae
Mark Skelton <mdskelton99@gmail.com>
2023-03-09 21:14:42 -0600

    Fix image alignment

--- src/components/markdown/MarkdownImage.tsx
+++ src/components/markdown/MarkdownImage.tsx
@@ -9,9 +9,9 @@ export async function MarkdownImage({ alt, src }: MarkdownImageProps) {
   const image = await import(`../../images/blog/${src}`)
 
   return (
-    <span className="inline-block text-center">
+    <span className="inline-flex w-full flex-col items-center text-center">
       <Image alt={alt ?? ""} placeholder="blur" src={image.default} />
-      <span className="text-muted inline-block text-sm">{alt}</span>
+      <span className="text-muted mt-3 inline-block text-sm">{alt}</span>
     </span>
   )
 }
