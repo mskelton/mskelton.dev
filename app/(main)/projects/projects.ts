19f3870486b28e16d386dc60bc1a19d36f584132
Mark Skelton <mdskelton99@gmail.com>
2023-07-17 15:32:54 -0500

    Fix ESLint config

--- app/(main)/projects/projects.ts
+++ app/(main)/projects/projects.ts
@@ -1,9 +1,9 @@
 import { siteMeta } from "../../lib/siteMeta"
+import logoDtsfmt from "./images/dtsfmt.svg"
 import logoSort from "./images/eslint-plugin-sort.svg"
+import logoFlashlight from "./images/flashlight.svg"
 import logoOneDark from "./images/one-dark.svg"
 import logoPlaywright from "./images/playwright.svg"
-import logoFlashlight from "./images/flashlight.svg"
-import logoDtsfmt from "./images/dtsfmt.svg"
 import logoRatchet from "./images/ratchet.svg"
 import logoTermicons from "./images/termicons.svg"
 import logoYarn from "./images/yarn.svg"
@@ -15,49 +15,49 @@ const npm = (name: string) => `https://www.npmjs.com/package/${name}`
 export const projects = [
   {
     description: `The 2nd most popular JetBrains theme and the only project I created that someone else maintains.`,
+    footer: true,
     href: gh("one-dark/jetbrains-one-dark-theme"),
     logo: logoOneDark,
     name: "JetBrains One Dark Theme",
     shortName: "One Dark Theme",
-    footer: true,
   },
   {
     description: `Yarn plugin to show outdated dependencies with way more features than anyone needs.`,
+    footer: true,
     href: gh("mskelton/yarn-plugin-outdated"),
     logo: logoYarn,
     name: "Yarn Outdated Plugin",
     shortName: "Yarn Outdated",
-    footer: true,
   },
   {
     description: `ESLint plugin for sorting imports, object properties, and more. Did I mention auto-fix that always works?`,
+    footer: true,
     href: npm("eslint-plugin-sort"),
     logo: logoSort,
     name: "ESLint Sort Plugin",
     shortName: "ESLint Sort",
-    footer: true,
   },
   {
     description: `ESLint plugin for Playwright testing with lots of rules to help you enforce good testing practices.`,
+    footer: true,
     href: npm("eslint-plugin-playwright"),
     logo: logoPlaywright,
     name: "ESLint Playwright Plugin",
     shortName: "ESLint Playwright",
-    footer: true,
   },
   {
     description: `Material style icons built for terminal rendering and a Neovim companion plugin.`,
+    footer: true,
     href: mskelton("termicons"),
     logo: logoTermicons,
     name: "Termicons",
-    footer: true,
   },
   {
     description: `Automatically convert React PropTypes to TypeScript definitions.`,
+    footer: true,
     href: mskelton("ratchet"),
     logo: logoRatchet,
     name: "Ratchet",
-    footer: true,
   },
   {
     description: `Find usages of imported symbols in your codebase.`,
