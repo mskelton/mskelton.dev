9de9fb2c3638c8988b7b8f121fdf017abff84d9e
Mark Skelton <mdskelton99@gmail.com>
2023-09-03 22:34:08 -0500

    Fix build and error page

--- app/(main)/bytes/[slug]/not-found.tsx
+++ app/(main)/bytes/[slug]/not-found.tsx
@@ -14,7 +14,7 @@ export default function ByteNotFound() {
       }
       code={404}
       subtitle="Seems you are trying to access a byte which doesn't exist. Try verifying that the URL is correct, or navigate back to the bytes list to find what you are looking for."
-      title="Byte not found"
+      title="Byte Not Found"
     />
   )
 }
