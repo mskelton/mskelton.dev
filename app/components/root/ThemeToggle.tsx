5fc255ffa04ae90a108daae63a24fff0a0f3d4f2
Mark Skelton <mdskelton99@gmail.com>
2023-09-06 18:10:16 -0500

    Update other tabs when changing theme

--- app/components/root/ThemeToggle.tsx
+++ app/components/root/ThemeToggle.tsx
@@ -1,10 +1,23 @@
 "use client"
 
 import { MoonIcon, SunIcon } from "@heroicons/react/20/solid"
-import { toggleTheme } from "../../lib/themeEffect"
+import { useEffect } from "react"
+import { themeEffect, toggleTheme } from "../../lib/themeEffect"
 import HeaderIconButton from "./HeaderIconButton"
 
 export function ThemeToggle() {
+  // React to storage changes in other tabs
+  useEffect(() => {
+    function handleStorageChange(event: StorageEvent) {
+      if (event.key === "theme") {
+        themeEffect()
+      }
+    }
+
+    window.addEventListener("storage", handleStorageChange)
+    return () => window.removeEventListener("storage", handleStorageChange)
+  })
+
   return (
     <HeaderIconButton aria-label="Toggle dark mode" onClick={toggleTheme}>
       <SunIcon className="dark:hidden" />
