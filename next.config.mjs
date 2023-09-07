f29bac45799fda2cd48b756c7303672cff800c68
Mark Skelton <mdskelton99@gmail.com>
2023-09-07 07:08:09 -0500

    Try this

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,9 @@ const nextConfig = {
   eslint: {
     dirs: ["app", "components", "config", "e2e"],
   },
+  experimental: {
+    serverComponentsExternalPackages: ["shiki"],
+  },
   images: {
     minimumCacheTTL: 3600, // 1 hour
   },
