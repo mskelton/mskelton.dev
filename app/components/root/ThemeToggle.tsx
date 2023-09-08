c4867dd222ddb6ab36b31514898229a4dd3c5d50
Mark Skelton <mdskelton99@gmail.com>
2023-09-08 14:14:49 -0500

    Add system theme

--- app/components/root/ThemeToggle.tsx
+++ app/components/root/ThemeToggle.tsx
@@ -1,11 +1,35 @@
 "use client"
 
-import { MoonIcon, SunIcon } from "@heroicons/react/20/solid"
-import { useEffect } from "react"
-import { themeEffect, toggleTheme } from "../../lib/themeEffect"
+import { Listbox, Transition } from "@headlessui/react"
+import {
+  ComputerDesktopIcon,
+  MoonIcon,
+  SunIcon,
+} from "@heroicons/react/20/solid"
+import { Fragment, useEffect, useState } from "react"
+import { twMerge } from "tailwind-merge"
+import {
+  FloatingOverlay,
+  FloatingPortal,
+  useFloating,
+} from "@floating-ui/react"
+import { themeEffect } from "../../lib/themeEffect"
 import HeaderIconButton from "./HeaderIconButton"
 
+const options = [
+  { Icon: ComputerDesktopIcon, label: "System", value: null },
+  { Icon: MoonIcon, label: "Dark", value: "dark" },
+  { Icon: SunIcon, label: "Light", value: "light" },
+]
+
 export function ThemeToggle() {
+  const [preference, setPreference] = useState<string | null>(null)
+  const { refs, strategy, x, y } = useFloating({
+    middleware: [],
+    placement: "bottom-end",
+    strategy: "fixed",
+  })
+
   // React to storage changes in other tabs
   useEffect(() => {
     function handleStorageChange(event: StorageEvent) {
@@ -16,12 +40,100 @@ export function ThemeToggle() {
 
     window.addEventListener("storage", handleStorageChange)
     return () => window.removeEventListener("storage", handleStorageChange)
-  })
+  }, [])
+
+  useEffect(() => {
+    setPreference(localStorage.getItem("theme"))
+
+    const matchMedia = window.matchMedia("(prefers-color-scheme: dark)")
+    matchMedia.addEventListener("change", themeEffect)
+    return () => matchMedia.removeEventListener("change", themeEffect)
+  }, [])
+
+  function handleChange(value: string) {
+    if (value === null) {
+      localStorage.removeItem("theme")
+    } else {
+      localStorage.setItem("theme", value)
+    }
+
+    setPreference(value)
+  }
 
   return (
-    <HeaderIconButton aria-label="Toggle dark mode" onClick={toggleTheme}>
-      <SunIcon className="dark:hidden" />
-      <MoonIcon className="hidden dark:block" />
-    </HeaderIconButton>
+    <Listbox
+      as="div"
+      className="relative inline-block text-left"
+      onChange={handleChange}
+      value={preference}
+    >
+      {({ open }) => (
+        <>
+          <Listbox.Button
+            ref={refs.setReference}
+            aria-label="Set website theme"
+            as={HeaderIconButton}
+          >
+            <SunIcon className="dark:hidden" />
+            <MoonIcon className="hidden dark:block" />
+          </Listbox.Button>
+
+          <FloatingPortal>
+            <Transition
+              // Run the theme effect after the transition exists. This ensures
+              // that we don't get any unwanted flashing when the menu closes.
+              afterLeave={themeEffect}
+              as={Fragment}
+              enter="transition duration-100 ease-out"
+              enterFrom="transform scale-95 opacity-0"
+              enterTo="transform scale-100 opacity-100"
+              leave="transition duration-0 ease-out"
+              leaveFrom="transform scale-100 opacity-100"
+              leaveTo="transform scale-95 opacity-0"
+              show={open}
+            >
+              <Listbox.Options
+                ref={refs.setFloating}
+                className="absolute right-0 z-[60] mt-2 w-56 rounded-xl bg-white dark:bg-zinc-800 shadow-lg ring-1 ring-zinc-900 ring-opacity-5 focus:outline-none"
+                static
+                style={{
+                  left: x ?? 0,
+                  position: strategy,
+                  top: y ?? 0,
+                }}
+              >
+                <div className="p-1">
+                  {options.map((option) => (
+                    <Listbox.Option
+                      key={option.label}
+                      className={({ active, selected }) =>
+                        twMerge(
+                          "group flex items-center px-4 py-2 text-xs font-medium w-full rounded-lg",
+                          active
+                            ? "bg-zinc-100 text-zinc-900 dark:bg-zinc-900 dark:text-zinc-100"
+                            : "text-zinc-700 dark:text-zinc-300",
+                          selected && "text-indigo-700 dark:text-indigo-300",
+                        )
+                      }
+                      value={option.value}
+                    >
+                      <option.Icon
+                        aria-hidden="true"
+                        className="mr-3 h-5 w-5 text-current"
+                      />
+
+                      {option.label}
+                    </Listbox.Option>
+                  ))}
+                </div>
+              </Listbox.Options>
+            </Transition>
+          </FloatingPortal>
+
+          {/* Lock scrolling to prevent the header overlapping when the toggle is open */}
+          {open ? <FloatingOverlay lockScroll /> : null}
+        </>
+      )}
+    </Listbox>
   )
 }
