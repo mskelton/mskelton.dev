8bddfa186e7040970eb7d42853bfec6157e1d874
Mark Skelton <mdskelton99@gmail.com>
2023-09-07 23:05:17 -0500

    Disable bytes in the nav for now, soft launching it

--- app/components/root/MobileNavigation.tsx
+++ app/components/root/MobileNavigation.tsx
@@ -57,7 +57,7 @@ export function MobileNavigation({ className }: MobileNavigationProps) {
               <nav className="mt-6">
                 <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
                   <MobileNavItem href="/blog">Blog</MobileNavItem>
-                  <MobileNavItem href="/bytes">Bytes</MobileNavItem>
+                  {/* <MobileNavItem href="/bytes">Bytes</MobileNavItem> */}
                   <MobileNavItem href="/about">About</MobileNavItem>
                   <MobileNavItem href="/projects">Projects</MobileNavItem>
                   <MobileNavItem href="/uses">Uses</MobileNavItem>
