6c527b127ce4673abc47f9a6cddf8c449e7efd5d
Mark Skelton <mdskelton99@gmail.com>
2023-09-09 12:31:57 -0500

    Update nav item

--- app/components/root/NavItem.tsx
+++ app/components/root/NavItem.tsx
@@ -1,27 +1,14 @@
-"use client"
-
 import clsx from "clsx"
 import { Route } from "next"
 import Link from "next/link"
-import { usePathname } from "next/navigation"
-import { useEffect, useState } from "react"
+import { LinkProps } from "next/link"
 
-export interface NavItemProps {
+export interface NavItemProps extends LinkProps<Route> {
   children: React.ReactNode
-  href: Route
+  isActive?: boolean
 }
 
-export function NavItem({ children, href }: NavItemProps) {
-  const [isActive, setIsActive] = useState(false)
-  const pathname = usePathname()
-
-  // Because the header is a different component between home and other pages,
-  // we have to set the active state after the initial render to ensure the
-  // underline transition is applied.
-  useEffect(() => {
-    setIsActive(pathname?.startsWith(href))
-  }, [pathname, href])
-
+export function NavItem({ children, isActive, ...props }: NavItemProps) {
   return (
     <li>
       <Link
@@ -30,9 +17,9 @@ export function NavItem({ children, href }: NavItemProps) {
           "relative flex gap-1 px-4 py-1 text-zinc-800 transition-colors hover:text-black dark:text-zinc-200 dark:hover:text-white",
           "after:absolute after:bottom-0.5 after:left-1/2 after:h-0.5 after:w-0 after:rounded-sm after:bg-indigo-500 after:transition-all aria-current:after:left-3.5 aria-current:after:right-3 aria-current:after:w-[calc(100%-32px)] dark:after:bg-indigo-400",
         )}
-        href={href}
+        {...props}
       >
-        {children}
+        <span className="z-10">{children}</span>
       </Link>
     </li>
   )
