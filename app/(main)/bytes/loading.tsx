a477c2485c60193f6644642fa2ee31fa483fa902
Mark Skelton <mdskelton99@gmail.com>
2023-09-09 12:32:05 -0500

    Loading page

---
+++ app/(main)/bytes/loading.tsx
@@ -0,0 +1,7 @@
+import Link from "next/link"
+import { Button } from "components/Button"
+import ErrorPage from "components/ErrorPage"
+
+export default function BytesLoading() {
+  return <p>Loading</p>
+}
