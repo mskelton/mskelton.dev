2ec91c4cf7aadf0ee2abbf0b679eb3a92204697c
Mark Skelton <mdskelton99@gmail.com>
2023-09-09 12:59:59 -0500

    Fix lint

--- app/(main)/bytes/loading.tsx
+++ app/(main)/bytes/loading.tsx
@@ -1,7 +1,3 @@
-import Link from "next/link"
-import { Button } from "components/Button"
-import ErrorPage from "components/ErrorPage"
-
 export default function BytesLoading() {
   return <p>Loading</p>
 }
