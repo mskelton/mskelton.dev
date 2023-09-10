64c3824c4c2a191618ddc21148eb51cabda0f9fa
Mark Skelton <mdskelton99@gmail.com>
2023-09-09 22:04:26 -0500

    Sync

--- scripts/setup-bun.sh
+++ scripts/setup-bun.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-curl -fsSL https://bun.sh/install | bash
-
-export BUN_INSTALL="$HOME/.bun"
-export PATH="$BUN_INSTALL/bin:$PATH"
-
-echo "$PATH"
+npm i -g bun
 which bun
+# curl -fsSL https://bun.sh/install | bash
+#
+# export BUN_INSTALL="$HOME/.bun"
+# export PATH="$BUN_INSTALL/bin:$PATH"
+#
+# echo "$PATH"
+# which bun
