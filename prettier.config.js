39a6350e97686963b9b887c646d21527e9d1e4a2
Mark Skelton <mdskelton99@gmail.com>
2023-09-29 23:59:44 -0500

    Prettier order

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,9 @@
 const config = {
+  cssDeclarationSorterOrder: "alphabetical",
   plugins: [
     "./node_modules/prettier-plugin-jsdoc/dist/index.js",
     "prettier-plugin-tailwindcss",
+    "prettier-plugin-css-order",
   ],
   proseWrap: "always",
   semi: false,
