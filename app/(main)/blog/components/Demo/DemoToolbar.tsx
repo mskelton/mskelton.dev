16f8f194cfa620cd8aa1ba4ca455aa99e5d14671
Mark Skelton <mdskelton99@gmail.com>
2023-09-30 20:43:33 -0500

    Add Tooltip component

--- app/(main)/blog/components/Demo/DemoToolbar.tsx
+++ app/(main)/blog/components/Demo/DemoToolbar.tsx
@@ -5,6 +5,7 @@ import {
   ViewfinderCircleIcon,
 } from "@heroicons/react/24/outline"
 import { GitHubIcon } from "components/SocialIcons"
+import { TooltipGroup } from "components/Tooltip"
 import DemoToolbarButton from "./DemoToolbarButton"
 
 const gh = (path: string) =>
@@ -32,28 +33,30 @@ export default function DemoToolbar({
   return (
     <div className="not-prose w-full rounded-b-xl border-t border-zinc-200 transition-colors dark:border-zinc-700/80">
       <div className="flex items-center justify-end gap-3 px-4 py-2">
-        <DemoToolbarButton onClick={onToggleExpanded} title="Expand code">
-          <ChevronUpDownIcon />
-        </DemoToolbarButton>
+        <TooltipGroup>
+          <DemoToolbarButton onClick={onToggleExpanded} title="Expand code">
+            <ChevronUpDownIcon />
+          </DemoToolbarButton>
 
-        <DemoToolbarButton onClick={() => copy(raw)} title="Copy the source">
-          <ClipboardDocumentIcon />
-        </DemoToolbarButton>
+          <DemoToolbarButton onClick={() => copy(raw)} title="Copy the source">
+            <ClipboardDocumentIcon />
+          </DemoToolbarButton>
 
-        <DemoToolbarButton href={gh(path)} title="View source on GitHub">
-          <GitHubIcon />
-        </DemoToolbarButton>
+          <DemoToolbarButton href={gh(path)} title="View source on GitHub">
+            <GitHubIcon />
+          </DemoToolbarButton>
 
-        <DemoToolbarButton
-          onClick={onFocusReset}
-          title="Reset focus to test keyboard navigation"
-        >
-          <ViewfinderCircleIcon />
-        </DemoToolbarButton>
+          <DemoToolbarButton
+            onClick={onFocusReset}
+            title="Reset focus to test keyboard navigation"
+          >
+            <ViewfinderCircleIcon />
+          </DemoToolbarButton>
 
-        <DemoToolbarButton onClick={onReset} title="Reset demo">
-          <ArrowPathIcon />
-        </DemoToolbarButton>
+          <DemoToolbarButton onClick={onReset} title="Reset demo">
+            <ArrowPathIcon />
+          </DemoToolbarButton>
+        </TooltipGroup>
       </div>
     </div>
   )
--- app/(main)/blog/components/Demo/DemoToolbarButton.tsx
+++ app/(main)/blog/components/Demo/DemoToolbarButton.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx"
 import { cloneElement } from "react"
+import { Tooltip, TooltipContent, TooltipTrigger } from "components/Tooltip"
 
 export interface DemoToolbarButtonProps {
   children: React.ReactElement
@@ -12,21 +13,27 @@ export default function DemoToolbarButton({
   children,
   href,
   onClick,
+  title,
 }: DemoToolbarButtonProps) {
   const Component = href ? "a" : "button"
 
   return (
-    <Component
-      className="rounded-full p-2 transition-[background-color] hover:bg-zinc-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 active:bg-indigo-100 dark:hover:bg-zinc-800 dark:active:bg-indigo-900"
-      href={href}
-      onClick={onClick}
-      rel={href ? "noopener noreferrer" : undefined}
-      target={href ? "_blank" : undefined}
-      type={href ? undefined : "button"}
-    >
-      {cloneElement(children, {
-        className: clsx("w-5 h-5", children.props.className),
-      })}
-    </Component>
+    <Tooltip>
+      <TooltipTrigger>
+        <Component
+          className="rounded-full p-2 transition-[background-color] hover:bg-zinc-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 active:bg-indigo-100 dark:hover:bg-zinc-800 dark:active:bg-indigo-900"
+          href={href}
+          onClick={onClick}
+          rel={href ? "noopener noreferrer" : undefined}
+          target={href ? "_blank" : undefined}
+          type={href ? undefined : "button"}
+        >
+          {cloneElement(children, {
+            className: clsx("w-5 h-5", children.props.className),
+          })}
+        </Component>
+      </TooltipTrigger>
+      <TooltipContent>{title}</TooltipContent>
+    </Tooltip>
   )
 }
