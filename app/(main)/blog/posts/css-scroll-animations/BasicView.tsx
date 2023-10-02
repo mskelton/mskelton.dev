218f6bd0e13b68d9ee6abd98e7e11c2b0371877f
Mark Skelton <mdskelton99@gmail.com>
2023-10-02 08:23:35 -0500

    More

---
+++ app/(main)/blog/posts/css-scroll-animations/BasicView.tsx
@@ -0,0 +1,11 @@
+import "./BasicView.css"
+
+export default function Component() {
+  return (
+    <div className="view-parent">
+      <div className="view-area">
+        <div className="view-box rounded-lg bg-blue-400" />
+      </div>
+    </div>
+  )
+}
