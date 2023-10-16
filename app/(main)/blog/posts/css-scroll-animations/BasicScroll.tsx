b1a39a25997c675312640ec55c873248c4bf1cfc
Mark Skelton <mdskelton99@gmail.com>
2023-10-15 22:53:16 -0500

    Content

--- app/(main)/blog/posts/css-scroll-animations/BasicScroll.tsx
+++ app/(main)/blog/posts/css-scroll-animations/BasicScroll.tsx
@@ -1,10 +1,8 @@
-import "./BasicScroll.css"
-
 export default function Component() {
   return (
-    <div className="scroll-parent">
-      <div className="scroll-area">
-        <div className="box rounded-lg bg-blue-400" />
+    <div className="relative h-[320px] overflow-y-scroll">
+      <div className="h-[1200px]">
+        <div className="sticky left-[calc(50%-var(--size)/2)] top-[calc(50%-var(--size)/2)] aspect-square w-[--size] animate-[spin_1ms_linear] rounded-lg bg-blue-400 [--size:120px] [animation-timeline:scroll()]" />
       </div>
     </div>
   )
