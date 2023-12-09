0a99241953586a4386f428e421264820854247a7
Mark Skelton <mdskelton99@gmail.com>
2023-12-09 12:34:35 -0600

    Support hidden alt text

--- components/markdown/MarkdownImage.tsx
+++ components/markdown/MarkdownImage.tsx
@@ -6,18 +6,29 @@ export interface MarkdownImageProps {
   src?: string
 }
 
-export default async function MarkdownImage({ alt, src }: MarkdownImageProps) {
+export default async function MarkdownImage({
+  alt: altProp = "",
+  src,
+}: MarkdownImageProps) {
+  const isAltHidden = altProp?.startsWith("!")
+  const alt = isAltHidden ? altProp.slice(1) : altProp
+
   const image = isExternalImage(src)
     ? { default: src }
     : await import(`../../app/(main)/blog/posts/${src}`)
 
   return (
     <span className="flex w-full flex-col items-center text-center">
-      <ZoomableImage alt={alt ?? ""} src={image.default} />
+      <ZoomableImage alt={alt} src={image.default} />
 
-      <span aria-hidden="true" className="text-muted mt-3 inline-block text-sm">
-        {alt}
-      </span>
+      {isAltHidden ? null : (
+        <span
+          aria-hidden="true"
+          className="text-muted mt-3 inline-block text-sm"
+        >
+          {alt}
+        </span>
+      )}
     </span>
   )
 }
