88d9069a36f460a82af56f85baa20e09de3cb3d5
Mark Skelton <mdskelton99@gmail.com>
2024-02-03 12:58:00 -0600

    Run on macOS

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,11 +5,6 @@ import { fileURLToPath } from "node:url"
 expect.extend(axeMatchers)
 
 export default defineConfig({
-  expect: {
-    toHaveScreenshot: {
-      maxDiffPixelRatio: 0.25,
-    },
-  },
   forbidOnly: !!process.env.CI,
   fullyParallel: true,
   reporter: process.env.CI ? "dot" : "list",
