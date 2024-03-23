6a2add425b74f8d615c2855f9ef07947feb67a46
Mark Skelton <mdskelton99@gmail.com>
2024-03-22 23:21:19 -0500

    Seed

--- e2e/global-setup.ts
+++
@@ -1,5 +0,0 @@
-async function globalSetup() {
-  await fetch("http://localhost:3000/reindex", { method: "POST" })
-}
-
-export default globalSetup
---
+++ scripts/seed.ts
@@ -0,0 +1,16 @@
+import fs from "node:fs/promises"
+import { upsertByte } from "../app/lib/api/bytes"
+
+const filenames = await fs.readdir(new URL("../e2e/bytes", import.meta.url))
+
+const promises = filenames.map(async (filename) => {
+  const id = filename.replace(".md", "")
+  const source = await fs.readFile(
+    new URL(`../e2e/bytes/${filename}`, import.meta.url),
+    "utf-8",
+  )
+
+  await upsertByte(id, source)
+})
+
+await Promise.all(promises)
