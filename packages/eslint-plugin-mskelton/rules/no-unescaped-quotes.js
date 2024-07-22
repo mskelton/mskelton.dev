61b6ce1fd8a7ea8df00bc7b2ed22fd2a3ad8a07c
Mark Skelton <mdskelton99@gmail.com>
2024-07-22 06:00:59 -0500

    Ramp (#69)
    * Ramp
    
    * TS
    
    * Sync

--- packages/eslint-plugin-mskelton/rules/no-unescaped-quotes.js
+++ packages/eslint-plugin-mskelton/rules/no-unescaped-quotes.js
@@ -1,3 +1,4 @@
+// @ts-check
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { isJSX } = require("../utils/jsx")
 
@@ -6,13 +7,17 @@ const entities = [
   { alternatives: ["’", "‘"], char: "'" },
 ]
 
+/**
+ * @param {import("eslint").Rule.RuleContext} context
+ * @param {import("eslint").Rule.Node} node
+ */
 function reportUnescaped(context, node) {
-  const source = context.getSourceCode()
+  if (!node.loc) return
 
   // HTML entities are already escaped in node.value (as well as node.raw),
-  // so pull the raw text from context.getSourceCode()
+  // so pull the raw text from context.sourceCode
   for (let i = node.loc.start.line; i <= node.loc.end.line; i++) {
-    let rawLine = context.getSourceCode().lines[i - 1]
+    let rawLine = context.sourceCode.lines[i - 1]
     const start = i === node.loc.start.line ? node.loc.start.column : 0
     const end = i === node.loc.end.line ? node.loc.end.column : rawLine.length
 
@@ -32,7 +37,7 @@ function reportUnescaped(context, node) {
                 entity: entity.char,
               },
               fix: (fixer) => {
-                const rangeStart = source.getIndexFromLoc({
+                const rangeStart = context.sourceCode.getIndexFromLoc({
                   column: start + index,
                   line: i,
                 })
@@ -48,9 +53,11 @@ function reportUnescaped(context, node) {
   }
 }
 
-module.exports = {
+/** @type {import("eslint").Rule.RuleModule} */
+const rule = {
   create(context) {
     return {
+      /** @param {import("eslint").Rule.Node} node */
       "Literal, JSXText"(node) {
         if (isJSX(node.parent)) {
           reportUnescaped(context, node)
@@ -69,3 +76,5 @@ module.exports = {
     type: "suggestion",
   },
 }
+
+module.exports = rule
--- packages/eslint-plugin-mskelton/utils/jsx.js
+++ packages/eslint-plugin-mskelton/utils/jsx.js
@@ -1,5 +1,5 @@
 /**
- * @param {object} node - Node to check.
+ * @param {import("eslint").Rule.Node} node
  * @returns {boolean} Whether or not the node if a JSX element or fragment.
  */
 exports.isJSX = function (node) {
