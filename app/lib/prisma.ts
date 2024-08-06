863bace228bf5e6914d483d97be1e578fc20f9b3
Mark Skelton <mdskelton99@gmail.com>
2024-08-05 21:03:30 -0500

    Update sqlite

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,6 +1,18 @@
+import { createClient } from "@libsql/client"
+import { PrismaLibSQL } from "@prisma/adapter-libsql"
 import { PrismaClient } from "@prisma/client"
 import { singleton } from "./singleton"
 
-const prisma = singleton("prisma", () => new PrismaClient())
+const prisma = singleton("prisma", () => {
+  const libsql = createClient({
+    authToken: process.env.TURSO_AUTH_TOKEN,
+    url: process.env.TURSO_DATABASE_URL!,
+  })
+
+  const adapter = new PrismaLibSQL(libsql)
+  const prisma = new PrismaClient({ adapter })
+
+  return prisma
+})
 
 export default prisma
